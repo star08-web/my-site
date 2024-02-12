0abe83a8e5de0eacc1b03a46485612dcf6e91315
star08-web <87822123+star08-web@users.noreply.github.com>
2024-02-12 17:49:29 +0100

    Added branding page and dyslexia friendly mode (beta)

--- js/rainbow.js
+++ js/rainbow.js
@@ -1,42 +1,44 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    const gridContainer = document.querySelector('.grid');
-    const gridItems = [];
-    for (let i = 0; i < 2000; i++) {
-      const gridItem = document.createElement('div');
-      gridItem.classList.add('grid-item');
-      gridContainer.appendChild(gridItem);
-      gridItems.push(gridItem);
-    }
-    document.addEventListener('mousemove', handleMouseMove);
-    function handleMouseMove(event) {
-      gridItems.forEach((gridItem) => {
-        const isMouseOver = isMouseOverElement(event, gridItem);
+function initbg() {
+  const gridContainer = document.querySelector('.grid-bg');
+  const gridItems = [];
+  for (let i = 0; i < 2000; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+    gridItems.push(gridItem);
+  }
+  document.addEventListener('mousemove', handleMouseMove);
+}
 
-        if (isMouseOver) {
-          const randomColor = getColor();
-          gridItem.style.backgroundColor = randomColor;
-        }
-      });
-    }
+function handleMouseMove(event) {
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach((gridItem) => {
+    const isMouseOver = isMouseOverElement(event, gridItem);
 
-    function isMouseOverElement(event, element) {
-      const rect = element.getBoundingClientRect();
-      return (
-        event.clientX >= rect.left &&
-        event.clientX <= rect.right &&
-        event.clientY >= rect.top &&
-        event.clientY <= rect.bottom
-      );
+    if (isMouseOver) {
+      const randomColor = getColor();
+      gridItem.style.backgroundColor = randomColor;
     }
   });
+}
 
-  function getColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function isMouseOverElement(event, element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    event.clientX >= rect.left &&
+    event.clientX <= rect.right &&
+    event.clientY >= rect.top &&
+    event.clientY <= rect.bottom
+  );
+}
+
+function getColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-})();
+  return color;
+}
+
+(function() {document.addEventListener('DOMContentLoaded', initbg());})();
---
+++ js/utils.js
@@ -0,0 +1,29 @@
+function checkCookie(cookieName) {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.startsWith(cookieName + '=')) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function createCookie(cookieName, cookieValue, daysToExpire) {
+    var date = new Date();
+    date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+function setStyleTag(cssCode, id="") {
+    var styleTag = document.createElement('style');
+    styleTag.type = 'text/css';
+    styleTag.appendChild(document.createTextNode(cssCode));
+    document.head.appendChild(styleTag);
+    styleTag.id = id
+}
+
+function deleteCookie(cookieName) {
+    document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
